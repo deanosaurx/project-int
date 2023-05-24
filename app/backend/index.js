4e77bad26958e656982ea40fead22e42a5aecb73
Dean Sorie <deansorie@icloud.com>
2023-05-24 13:11:59 +0300

    checking something

--- app/backend/index.js
+++ app/backend/index.js
@@ -1,22 +1,22 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-import Connection from './database/db.js'; 
-import Routes from './routes/route.js';
+import Connection from "./database/db.js";
+import Routes from "./routes/route.js";
 
-const app = express()
-const port = 8000
+const app = express();
+const port = 8000;
 
 app.use(cors());
 
-app.use(bodyParser.json({extended: true}));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', Routes);
+app.use("/", Routes);
 
 Connection();
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
