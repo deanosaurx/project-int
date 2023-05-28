70bb05784bdae1433c36fae39e0d834338cefdb8
Dean Sorie <deansorie@icloud.com>
2023-05-28 15:24:08 +0300

    made some changes

--- infra/terraform/7-nodes.tf
+++ infra/terraform/7-nodes.tf
@@ -39,10 +39,10 @@ resource "aws_eks_node_group" "private-nodes" {
   ]
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.xlarge"]
+  instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 3
     min_size     = 0
   }
